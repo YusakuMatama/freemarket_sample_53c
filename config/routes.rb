edf7b8a6b0b74cd346f10b138292201c8ccb5a1a
YusakuMatama <kotatsu0715@gmail.com>
2019-06-27 15:46:32 +0900

    devise model sakusei

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root "tops#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :tops, only: [:index, :show]
