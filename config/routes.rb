7c29851a9be3b6b9ada599a09e43b9b91a9d0462
YusakuMatama <kotatsu0715@gmail.com>
2019-06-28 12:47:04 +0900

    ログインルートの修正

--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,6 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :tops, only: [:index, :show, :new, :edit]
+  resources :users, only: [:index]
+
 end
