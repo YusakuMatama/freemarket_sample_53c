288054139ca5d8640e18ce0c2001d6d58183f0b6
Bernkaste <akaneiro0721@gmail.com>
2019-07-01 20:15:53 +0900

    Revise payjp

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,15 +1,15 @@
 class ItemsController < ApplicationController
 
   def index
-    @top = "hoge"
+    
   end
 
   def new
-    @top = "hoge"
+    
   end
 
   def show
-    @item = Item.find(1)
+    
   end
 
   def sell
@@ -18,14 +18,16 @@ class ItemsController < ApplicationController
   def purchase
     
     # @item = Item.find(params[:item_id])
-    @item = Item.find(1)
+    @item = Item.find(1) #仮置き。実際は上の記述を使う
     
     Payjp.api_key = ENV['PAYJP_TEST_SECRET_KEY']
-    Payjp::Charge.create(currency: 'jpy', amount: 100, card: params['payjp-token'])
-
+    begin
+      Payjp::Charge.create(currency: 'jpy', amount: 100, card: params['payjp-token'])
+    rescue
+      redirect_to items_path
+    end
     
-    @item.update(buyer_id: 1, selled_at: "#{DateTime.now}")
-    
-    redirect_to '/items/1'
+    @item.update(buyer_id: 1, selled_at: "#{DateTime.now}") #buyer_idの値は仮置き
+    redirect_to '/items/1' #このパスは仮置き
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
-  # devise_for :users
+  devise_for :users
   root "tops#index"
-  # get 'mypage/identification' => 'users#identification'
+  get 'mypage/identification' => 'users#identification'
   get 'mypage/card' => 'users#card'
-  # get 'sell' => 'items#sell'
+  get 'sell' => 'items#sell'
 
-  # get  'new' => 'registrations#new'
-  # get  'address' => 'registrations#address'
-  # get  'create' => 'registrations#create'
+  get  'new' => 'registrations#new'
+  get  'address' => 'registrations#address'
+  get  'create' => 'registrations#create'
   get  'credit' => 'registrations#credit'
-  # get  'sns' => 'registrations#sns'
-  # get  'phone' => 'registrations#phone'
+  get  'sns' => 'registrations#sns'
+  get  'phone' => 'registrations#phone'
 
   
 
