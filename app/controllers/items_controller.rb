3e2ad7eb86c94de4cdcb5f9c8f47ea80ec257280
YusakuMatama <kotatsu0715@gmail.com>
2019-07-03 19:48:26 +0900

    複数画像投稿機能の実装（途中経過2:コメントアウトの消去）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,8 +19,6 @@ class ItemsController < ApplicationController
     @item.item_images.build
     @category = Category.all
     @brand = Brand.all
-
-    # @item.build_order_statu
   end
 
   def create
@@ -32,17 +30,9 @@ class ItemsController < ApplicationController
    
     if @item.save 
       params.require(:item).require(:item_images_attributes).each do |image|
-        Item.item_images.create(image: image, item_id: :id)
-        # ぷろぐらむ
-      end
-
-
+      Item.item_images.create(image: image, item_id: :id)
+    end
       @item_status = OrderStatus.create(status: 1, item_id: Item.all.last().id)
-
-      # if @params_images.present?
-      # ItemImage.create(@params_item)
-      # redirect_to root_path
-      # end
 
     else
       render :sell
@@ -50,47 +40,40 @@ class ItemsController < ApplicationController
   end
 
 private
-def items_params
-  @params_category = params.require(:item).require(:category_attributes).permit(:id)
-  # @params_category = @category.find(@params_category[:id])
+  def items_params
+    @params_category = params.require(:item).require(:category_attributes).permit(:id)
 
-  @params_brand = params.require(:item).require(:brand_attributes).permit(:name)
-  @params_brand = @brand.find_by(name: @params_brand[:name])
+    @params_brand = params.require(:item).require(:brand_attributes).permit(:name)
+    @params_brand = @brand.find_by(name: @params_brand[:name])
 
-  if @params_brand.present?
-    @params_brand = @params_brand[:id]
+    if @params_brand.present?
+      @params_brand = @params_brand[:id]
+    end
+
+    @params_item = params.require(:item).permit(:name, :detail, :condition, :delivery_cost, :delivery_prefecture, :days_to_ship, :delivery_method, :price, item_images_attributes: {image: []}).merge(user_id: 1, sales_condition: 1, category_id: @params_category[:id], brand_id: @params_brand)
+    params_int(@params_item)
   end
 
-  @params_item = params.require(:item).permit(:name, :detail, :condition, :delivery_cost, :delivery_prefecture, :days_to_ship, :delivery_method, :price, item_images_attributes: {image: []}).merge(user_id: 1, sales_condition: 1, category_id: @params_category[:id], brand_id: @params_brand)
-  params_int(@params_item)
-end
-
-def params_int(model_params)
-  model_params.each do |key,value|
-    unless key == "item_images_attributes" 
-      if integer_string?(value)
-        model_params[key] = value.to_i
+  def params_int(model_params)
+    model_params.each do |key,value|
+      unless key == "item_images_attributes" 
+        if integer_string?(value)
+          model_params[key] = value.to_i
+        end
+      end
+      if key == "item_images_attributes"
+        model_params[key] = value 
       end
     end
-    if key == "item_images_attributes"
-      model_params[key] = value 
+  end
+
+  def integer_string?(str)
+    if str.present?
+    Integer(str)
+    true
     end
+  rescue ArgumentError
+    false
   end
-end
-
-def integer_string?(str)
-  if str.present?
-  Integer(str)
-  true
-  end
-rescue ArgumentError
-  false
-end
-
-# def image_params
-#   if params.require(:item).require(:item_images_attributes).require("0").present?
-#     @params_image = params.require(:item).require(:item_images_attributes).require("0").permit(:image).merge(item_id: Item.all.last().id)
-#   end
-# end
 
 end
