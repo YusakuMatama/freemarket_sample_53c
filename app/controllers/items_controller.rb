c5579839fab26acba3fa10ded2a2e29aa7b1bc42
Jin Ruixin <audreysking@gmail.com>
2019-07-04 20:20:49 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -58,47 +58,47 @@ class ItemsController < ApplicationController
     @status.update(status: 3)
     redirect_to '/items/1' #このパスは仮置き
   end
-end
 
-private
-def items_params
-  @params_categories = params.require(:item).require(:category_attributes).permit(:id)
 
-  @params_brands = params.require(:item).require(:brand_attributes).permit(:name)
-  @params_brands = @brands.find_by(name: @params_brands[:name])
+  private
+  def items_params
+    @params_categories = params.require(:item).require(:category_attributes).permit(:id)
 
-  if @params_brands.present?
-    @params_brands = @params_brands[:id]
+    @params_brands = params.require(:item).require(:brand_attributes).permit(:name)
+    @params_brands = @brands.find_by(name: @params_brands[:name])
+
+    if @params_brands.present?
+      @params_brands = @params_brands[:id]
+    end
+
+    @params_items = params.require(:item).permit(:name, :detail, :condition, :delivery_cost, :delivery_prefecture, :days_to_ship, :delivery_method, :price, item_images_attributes: [:image]).merge(user_id: 1, sales_condition: 1, category_id: @params_categories[:id], brand_id: @params_brands)
+    params_int(@params_items)
   end
 
-  @params_items = params.require(:item).permit(:name, :detail, :condition, :delivery_cost, :delivery_prefecture, :days_to_ship, :delivery_method, :price, item_images_attributes: [:image]).merge(user_id: 1, sales_condition: 1, category_id: @params_categories[:id], brand_id: @params_brands)
-  params_int(@params_items)
-end
-
-def params_int(model_params)
-  model_params.each do |key,value|
-    unless key == "item_images_attributes" 
-      if integer_string?(value)
-        model_params[key] = value.to_i
+  def params_int(model_params)
+    model_params.each do |key,value|
+      unless key == "item_images_attributes" 
+        if integer_string?(value)
+          model_params[key] = value.to_i
+        end
+      end
+      if key == "item_images_attributes"
+        model_params[key] = value 
       end
     end
-    if key == "item_images_attributes"
-      model_params[key] = value 
+  end
+
+  def integer_string?(str)
+    if str.present?
+    Integer(str)
+    true
     end
+  rescue ArgumentError
+    false
   end
-end
 
-def integer_string?(str)
-  if str.present?
-  Integer(str)
-  true
+  def set_item
+    @item = Item.find(params[:id])
   end
-rescue ArgumentError
-  false
-end
-
-def set_item
-  @item = Item.find(params[:id])
-end
 
 end
