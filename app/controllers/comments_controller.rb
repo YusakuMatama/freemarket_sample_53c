106131b48a9d0e8ef93fdd83837313ad4a71a96a
Jin Ruixin <audreysking@gmail.com>
2019-07-10 14:25:01 +0900

    Update comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,12 @@
 class CommentsController < ApplicationController
   def create
-    Comment.create(content: comment_params[:content], item_id: comment_params[:item_id], user_id: current_user.id)
-
-    redirect_to item_path(comment_params[:item_id])
+    @comment = Comment.new(content: comment_params[:content], item_id: comment_params[:item_id], user_id: current_user.id)
+    if @comment.save
+      redirect_to item_path(comment_params[:item_id])
+    else
+      render :new
+    end
   end
-
 
   private
   def comment_params
