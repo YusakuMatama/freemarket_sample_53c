8d093d45c0a1a32ecc73856ff19c10c374eeb67e
Jin Ruixin <audreysking@gmail.com>
2019-07-11 19:31:40 +0900

    Update tops_controller.rb

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,14 +1,14 @@
 class TopsController < ApplicationController
 
   def index
-    @ladysitems = Item.all.eager_load(:category).where(categories:{grandparent_id: 1}).limit(4)
-    @mensitems = Item.all.eager_load(:category).where(categories:{grandparent_id: 2}).limit(4)
-    @babykidsitems = Item.all.eager_load(:category).where(categories:{grandparent_id: 3}).limit(4)
-    @cosmeitems = Item.all.eager_load(:category).where(categories:{grandparent_id: 7}).limit(4)
-    @zyaneruitems = Item.where(brand_id:1).limit(4)
-    @shufureemitems = Item.where(brand_id:4).limit(4)
-    @ruiwetonitems = Item.where(brand_id:3).limit(4)
-    @naigiitems = Item.where(brand_id:2).limit(4)
+    @ladysitems = Item.all.eager_load(:category).where(categories:{grandparent_id: 1}).order("items.id DESC").limit(4).includes(:item_images)
+    @mensitems = Item.all.eager_load(:category).where(categories:{grandparent_id: 2}).order("items.id DESC").limit(4).includes(:item_images)
+    @babykidsitems = Item.all.eager_load(:category).where(categories:{grandparent_id: 3}).order("items.id DESC").limit(4).includes(:item_images)
+    @cosmeitems = Item.all.eager_load(:category).where(categories:{grandparent_id: 7}).order("items.id DESC").limit(4).includes(:item_images)
+    @zyaneruitems = Item.where(brand_id:1).order("id DESC").limit(4).includes(:item_images)
+    @shufureemitems = Item.where(brand_id:4).order("id DESC").limit(4).includes(:item_images)
+    @ruiwetonitems = Item.where(brand_id:3).order("id DESC").limit(4).includes(:item_images)
+    @naigiitems = Item.where(brand_id:2).order("id DESC").limit(4).includes(:item_images)
   end
 
   def edit
