3bd7d2e3a33e46ea2015f69e282795910c623b99
Jin Ruixin <audreysking@gmail.com>
2019-07-15 14:10:54 +0900

    Update order_statuses_controller.rb

--- app/controllers/order_statuses_controller.rb
+++ app/controllers/order_statuses_controller.rb
@@ -1,8 +1,8 @@
 class OrderStatusesController < ApplicationController
   def update
-    @status = OrderStatus.find(params[:item_id])
-    if @status.status == 1 || @status.status == 2
-      if @status.status == 1
+    @status = OrderStatus.where(item_id: params[:item_id])
+    if @status[0].status == 1 || @status[0].status == 2
+      if @status[0].status == 1
         @status.update(status: 2)
       else
         @status.update(status: 1)
