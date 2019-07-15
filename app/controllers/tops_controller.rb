0640bcb382d8dd69a9d34264cc82dca5bfd77a41
Jin Ruixin <audreysking@gmail.com>
2019-07-15 19:07:02 +0900

    Update tops_controller.rb

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -2,13 +2,13 @@ class TopsController < ApplicationController
 
   def index
     @ladysitems = Item.eager_load(:category).eager_load(:order_status).where(categories:{grandparent_id: 1},order_statuses:{status: 1}).order("items.id DESC").limit(4).includes(:item_images)
-    @mensitems = Item.eager_load(:category).where(categories:{grandparent_id: 2}).order("items.id DESC").limit(4).includes(:item_images)
-    @babykidsitems = Item.eager_load(:category).where(categories:{grandparent_id: 3}).order("items.id DESC").limit(4).includes(:item_images)
-    @cosmeitems = Item.eager_load(:category).where(categories:{grandparent_id: 7}).order("items.id DESC").limit(4).includes(:item_images)
-    @zyaneruitems = Item.where(brand_id:1).order("id DESC").limit(4).includes(:item_images)
-    @shufureemitems = Item.where(brand_id:4).order("id DESC").limit(4).includes(:item_images)
-    @ruiwetonitems = Item.where(brand_id:3).order("id DESC").limit(4).includes(:item_images)
-    @naigiitems = Item.where(brand_id:2).order("id DESC").limit(4).includes(:item_images)
+    @mensitems = Item.eager_load(:category).eager_load(:order_status).where(categories:{grandparent_id: 2},order_statuses:{status: 1}).order("items.id DESC").limit(4).includes(:item_images)
+    @babykidsitems = Item.eager_load(:category).eager_load(:order_status).where(categories:{grandparent_id: 3},order_statuses:{status: 1}).order("items.id DESC").limit(4).includes(:item_images)
+    @cosmeitems = Item.eager_load(:category).eager_load(:order_status).where(categories:{grandparent_id: 7},order_statuses:{status: 1}).order("items.id DESC").limit(4).includes(:item_images)
+    @zyaneruitems = Item.eager_load(:order_status).where(brand_id:1,order_statuses:{status: 1}).order("items.id DESC").limit(4).includes(:item_images)
+    @shufureemitems = Item.eager_load(:order_status).where(brand_id:4,order_statuses:{status: 1}).order("items.id DESC").limit(4).includes(:item_images)
+    @ruiwetonitems = Item.eager_load(:order_status).where(brand_id:3,order_statuses:{status: 1}).order("items.id DESC").limit(4).includes(:item_images)
+    @naigiitems = Item.eager_load(:order_status).where(brand_id:2,order_statuses:{status: 1}).order("items.id DESC").limit(4).includes(:item_images)
   end
 
   def edit
