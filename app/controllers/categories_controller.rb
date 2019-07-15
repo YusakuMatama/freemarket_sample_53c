b5b7f66f45069aae1a65abe74b41918c951fc8a0
YusakuMatama <kotatsu0715@gmail.com>
2019-07-15 19:43:53 +0900

    ぱんくず途中経過

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,6 +3,8 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
     @items = @category.search
+    @brand = Brand.find(params[:id])
+
   end
 
 end
