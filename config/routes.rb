b92461edeb1b8f4cfdb56819e6d172f979c750e5
Jin Ruixin <audreysking@gmail.com>
2019-07-15 18:01:28 +0900

    update

--- config/routes.rb
+++ config/routes.rb
@@ -28,12 +28,12 @@ Rails.application.routes.draw do
     collection do
       get 'search'
     end
-    member do
-      get 'category'
-    end
-    member do
-      get 'brand'
-    end
+    # member do
+    #   get 'category'
+    # end
+    # member do
+    #   get 'brand'
+    # end
     collection do
       get 'selling'
     end
